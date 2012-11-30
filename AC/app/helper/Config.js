e2b1ad7f86b2fea903e88892f71a6c8917bdae90
gereXciTer <vladimir.gartsev@gmail.com>
2012-11-30 18:53:02 +0300

    psd

--- AC/app/helper/Config.js
+++ AC/app/helper/Config.js
@@ -2,7 +2,7 @@ Ext.define('AC.helper.Config', {
 	singleton : true,
 	
     title: 'TestApp',
-    apiUrl: (window.location.href.search('autocron.ru') > 0) ? 'http://autocron.ru/api/' : 'http://localhost/api/',
+    apiUrl: (window.location.href.search('autocron.ru') > 0) ? 'http://autocron.ru/ac/api/' : 'http://localhost/ac/api/',
     carImagesUrl: 'http://autocron.ru/images/cars/',
     headersAppId: 'AUTOCRON',
     autocrontoken : sessionStorage.getItem('ACUserKey'),
